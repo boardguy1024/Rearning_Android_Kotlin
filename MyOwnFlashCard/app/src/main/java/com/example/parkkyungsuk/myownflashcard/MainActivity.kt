1fc410fd58bb81b4f0156013b3b07347b31a64cd
park kyung suk <boardguy1024@gmail.com>
2018-08-26 17:00:56 +0900

    MyOwnFlashCard: WordListViewの基本作成

--- MyOwnFlashCard/app/src/main/java/com/example/parkkyungsuk/myownflashcard/MainActivity.kt
+++ MyOwnFlashCard/app/src/main/java/com/example/parkkyungsuk/myownflashcard/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.parkkyungsuk.myownflashcard
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonEdit.setOnClickListener {
+            val intent = Intent(this@MainActivity, EditActivity::class.java)
+            startActivity(intent)
+        }
+        buttonColor1.setOnClickListener {
+            constraintLayout.setBackgroundResource(R.color.color1.toInt())
+        }
+        buttonColor2.setOnClickListener {
+            constraintLayout.setBackgroundResource(R.color.color2.toInt())
+        }
+        buttonColor3.setOnClickListener {
+            constraintLayout.setBackgroundResource(R.color.color3.toInt())
+        }
+        buttonColor4.setOnClickListener {
+            constraintLayout.setBackgroundResource(R.color.color4.toInt())
+        }
+        buttonColor5.setOnClickListener {
+            constraintLayout.setBackgroundResource(R.color.color5.toInt())
+        }
+        buttonColor6.setOnClickListener {
+            constraintLayout.setBackgroundResource(R.color.color6.toInt())
+        }
+
     }
 }
--- MyOwnFlashCard/app/src/main/java/com/example/parkkyungsuk/myownflashcard/WordListActivity.kt
+++ MyOwnFlashCard/app/src/main/java/com/example/parkkyungsuk/myownflashcard/WordListActivity.kt
@@ -1,12 +1,25 @@
 package com.example.parkkyungsuk.myownflashcard
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_word_list.*
 
 class WordListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_list)
+
+        buttonAddWords.setOnClickListener {
+            val intent = Intent(this@WordListActivity, EditActivity::class.java)
+            intent.putExtra(getString(R.string.intent_key_status), getString(R.string.status_add))
+            startActivity(intent)
+        }
+
+        buttonBack.setOnClickListener {
+            finish()
+        }
+
     }
 }
