7bf36e24aa9a60aec8f458fd6390709d838ce673
park kyung suk <boardguy1024@gmail.com>
2018-08-26 14:32:00 +0900

    MyOwnFlashCard: firstInit

---
+++ MyOwnFlashCard/app/src/main/java/com/example/parkkyungsuk/myownflashcard/WordListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.parkkyungsuk.myownflashcard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WordListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_list)
+    }
+}
